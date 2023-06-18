63ec35df7d7c0463d0601065299234fca9260afd
Daniela Liendo <daniela@ualabee.com>
2023-06-18 21:39:54 +0200

    MapsModule was created and MapScreenComponent was declared and exported

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MapScreenComponent } from './screens/map-screen/map-screen.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MapScreenComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MapScreenComponent
   ]
 })
 
