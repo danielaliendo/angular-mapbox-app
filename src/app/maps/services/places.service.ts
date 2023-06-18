dd88831bf39138d6419ac5643d8b83e364670806
Daniela Liendo <daniela@ualabee.com>
2023-06-18 21:59:13 +0200

    getUserLocation method was added

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -5,5 +5,39 @@ import { Injectable } from '@angular/core';
 })
 
 export class PlacesService {
-  constructor() {}
+
+  public userLocation?: [number, number]
+
+  get isUserLocationReady(): boolean {
+    return !!this.userLocation
+  }
+
+  constructor() {
+    this.getUserLocation()
+  }
+
+  public async getUserLocation(): Promise<[number, number]> {
+    return new Promise((resolve, reject) => {
+      if (this.userLocation) {
+        resolve(this.userLocation)
+      } else {
+        navigator.geolocation.getCurrentPosition(
+          ({coords}) => {
+            this.userLocation = [coords.longitude, coords.latitude]
+            resolve(this.userLocation)
+          },
+          (error) => {
+            alert('Error getting your location')
+            reject(error)
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+          }
+        )
+      }
+    })
+  }
+
 }
