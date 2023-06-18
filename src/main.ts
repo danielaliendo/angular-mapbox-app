57b43802c0ae51ad47ef2d2dcb8c41f256942a1a
Daniela Liendo <daniela@ualabee.com>
2023-06-18 21:38:48 +0200

    exception was added when browser does not support geolocation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if (!navigator.geolocation) {
+  alert('Geolocation is not supported by your browser');
+  throw new Error('Geolocation is not supported by your browser');
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
