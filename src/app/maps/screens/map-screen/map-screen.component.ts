2637e70e77cccb8869d4c725f43520db49d3ecd6
Daniela Liendo <daniela@ualabee.com>
2023-06-18 21:38:06 +0200

    map screen page component was created

---
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maps-map-screen',
+  templateUrl: './map-screen.component.html',
+  styleUrls: ['./map-screen.component.css']
+})
+
+export class MapScreenComponent {
+
+}
