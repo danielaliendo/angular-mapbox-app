86c6f818c2361f3c8c780993c1fe91a4a4f48d54
Daniela Liendo <daniela@ualabee.com>
2023-06-18 22:09:37 +0200

    map view component was created

---
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maps-map-view',
+  templateUrl: './map-view.component.html',
+  styleUrls: ['./map-view.component.css']
+})
+
+export class MapViewComponent {
+
+}
